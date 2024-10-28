8bfaffb3b09c24de63eff7b62cb8b890485ff492
Rishi <rishi491991@gmail.com>
2024-10-28 14:39:48 +0530

    added v-dialog

--- src/pages/UserListing/userListingPage.vue
+++ src/pages/UserListing/userListingPage.vue
@@ -2,6 +2,12 @@
 import { ref } from "vue";
 import api from "@/services/api";
 const search = ref(null);
+const isDescriptionModalOpen = ref(false);
+const dialogIndex = ref(null);
+const showDescriptionDialog = (index) => {
+  dialogIndex.value = index;
+  isDescriptionModalOpen.value = true;
+};
 const searchedUserList = ref(null);
 const usersList = ref([]);
 const fetchUsers = async () => {
@@ -35,8 +41,13 @@ await fetchUsers();
       ></v-text-field>
     </v-app-bar>
     <v-row align="center" dense>
-      <v-col cols="12" md="4" v-for="user in searchedUserList">
-        <v-card class="mx-auto" :subtitle="user.username" :title="user.name">
+      <v-col cols="12" md="4" v-for="(user, index) in searchedUserList">
+        <v-card
+          class="mx-auto"
+          :subtitle="user.username"
+          :title="user.name"
+          @click="showDescriptionDialog(index)"
+        >
           <template v-slot:prepend>
             <!-- <v-icon color="primary" icon="mdi-account"></v-icon> -->
             <img
@@ -58,5 +69,34 @@ await fetchUsers();
         </v-card>
       </v-col>
     </v-row>
+    <v-dialog v-model="isDescriptionModalOpen" width="1024">
+      <v-card class="mx-auto" subtitle="user.username" title="user.name">
+        <template v-slot:prepend>
+          <!-- <v-icon color="primary" icon="mdi-account"></v-icon> -->
+          <img
+            :src="`https://randomuser.me/api/portraits/women/${usersList[dialogIndex].id}.jpg`"
+            height="50px"
+            width="50px"
+            class="rounded-xl"
+          />
+        </template>
+        <v-card-text>
+          <v-chip color="green" label>
+            {{ usersList[dialogIndex].address.street }},
+            {{ usersList[dialogIndex].address.suite }},
+            {{ usersList[dialogIndex].address.city }},
+            {{ usersList[dialogIndex].address.zipcode }}
+          </v-chip>
+          <br />
+          email: {{ usersList[dialogIndex].email }}
+          <br />
+          phone: {{ usersList[dialogIndex].phone }}
+          <br />
+          company: {{ usersList[dialogIndex].company.name }},
+          {{ usersList[dialogIndex].company.catchPhrase }},
+          {{ usersList[dialogIndex].company.bs }}
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </main>
 </template>
